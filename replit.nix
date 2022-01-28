eb8311cd635e61a4751f13a13bffdea21985f1a0
Anastasia Romanov <86816930+Anastasia-Romanov@users.noreply.github.com>
2022-01-28 21:42:51 +0000

    chore: add eslint

--- replit.nix
+++ replit.nix
@@ -1,8 +1,9 @@
 { pkgs }: {
     deps = [
         pkgs.bashInteractive
+        pkgs.vim
         pkgs.nodejs-16_x
         pkgs.nodePackages.pnpm
-        pkgs.vim
+        pkgs.nodePackages.eslint
     ];
 }
