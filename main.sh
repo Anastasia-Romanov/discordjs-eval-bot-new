b78837f6ab3b62475ee6c3616053a736ee24e00d
doinkythederp <doinkythederp@icloud.com>
2022-04-04 20:20:37 -0700

    refactor: remove redundant comments

--- main.sh
+++ main.sh
@@ -1,15 +1,4 @@
-# thanks doinky
 while [ true ]; do
-  #if [ ! -f "./index.js" ]; then
-    #echo "index file doesn't exit, fetching from git..."
-    #if [ ! -d "./.git" ]; then
-    #  git init -q
-    #  git remote add origin https://github.com/TheNoob27/discord.js-eval-bot-
-    #  git fetch -q
-    #  git reset origin/master --hard -q
-    #fi
-  # git fetch -q
-  # git reset origin/master --hard -q
-  #fi
+  # we're trusting people not to blow up the filesystem here
   node --trace-warnings --trace-deprecation .
 done
