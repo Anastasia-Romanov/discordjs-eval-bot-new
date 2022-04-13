4643d79708b082f52f03b402c384e51aafbc73fa
doinkythederp <doinkythederp@icloud.com>
2022-04-12 18:52:52 -0700

    fix: move node options to env variable

--- main.sh
+++ main.sh
@@ -1,4 +1,4 @@
 while [ true ]; do
   # we're trusting people not to blow up the filesystem here
-  pnpm ts-node --trace-warnings --trace-deprecation src/index.ts
+  NODE_OPTIONS="--trace-warnings --trace-deprecation" pnpm ts-node src/index.ts
 done
