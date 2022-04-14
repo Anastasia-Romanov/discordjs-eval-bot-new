a4668e204a55f3cad77d5de63aeb984be4997828
Anastasia-Romanov <86816930+Anastasia-Romanov@users.noreply.github.com>
2022-04-14 13:55:12 +1200

    Revert "refactor: typescript rewrite"

--- main.sh
+++ main.sh
@@ -1,4 +1,15 @@
+# thanks doinky
 while [ true ]; do
-  # we're trusting people not to blow up the filesystem here
-  pnpm ts-node --trace-warnings --trace-deprecation src/index.ts
+  #if [ ! -f "./index.js" ]; then
+    #echo "index file doesn't exit, fetching from git..."
+    #if [ ! -d "./.git" ]; then
+    #  git init -q
+    #  git remote add origin https://github.com/TheNoob27/discord.js-eval-bot-
+    #  git fetch -q
+    #  git reset origin/master --hard -q
+    #fi
+  # git fetch -q
+  # git reset origin/master --hard -q
+  #fi
+  node --trace-warnings --trace-deprecation .
 done
