631331632198363d00daae0f56ad3409a6180c56
Anastasia Romanov <86816930+Anastasia-Romanov@users.noreply.github.com>
2022-07-10 01:19:12 +0000

    node 18, update packages

--- replit.nix
+++ replit.nix
@@ -2,7 +2,7 @@
     deps = [
         pkgs.bashInteractive
         pkgs.vim
-        pkgs.nodejs-17_x
+        pkgs.nodejs-18_x
         pkgs.nodePackages.pnpm
         pkgs.nodePackages.eslint
     ];
